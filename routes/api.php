92bf88f1df55db00e6c8b900536a99b6eb42125c
shubham <shubhamjaiswal7171@gmail.com>
2024-05-24 16:20:50 +0530

    Apis of lite apps update

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,14 @@ Route::controller(RegisterController::class)->group(function(){
     Route::post('login', 'login');
 });
 
-
+Route::resource('apps', LightAppController::class);
+	Route::get('app_role_list', [LightAppController::class, 'AppRoleList']);
+	Route::post('apps-update/{id}', [LightAppController::class, 'update']);
+	Route::post('add-apps-desktop/{id}', [LightAppController::class, 'apps']);
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group( function () {
-	Route::resource('apps', LightAppController::class);
-	Route::get('app_role_list', [LightAppController::class, 'AppRoleList']);
-	Route::post('apps-update/{id}', [LightAppController::class, 'update']);
-	Route::post('add-apps-desktop/{id}', [LightAppController::class, 'apps']);
+	
 });
